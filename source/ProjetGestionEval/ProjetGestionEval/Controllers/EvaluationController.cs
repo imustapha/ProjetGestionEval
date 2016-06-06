9f1e91f866fa84d59654ed210c658f2d607422d3
igherra <igherra.mustapha@gmail.com>
2016-06-06 11:28:46 +0000

    list collaborateur evaluer et non evaluer

--- source/ProjetGestionEval/ProjetGestionEval/Controllers/EvaluationController.cs
+++ source/ProjetGestionEval/ProjetGestionEval/Controllers/EvaluationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,11 +10,18 @@ namespace ProjetGestionEval.Controllers
     public class EvaluationController : Controller
     {
         // GET: Evaluation
+
+        static string ConnectionString = "database=bd_gestion;server=localhost;uid=root";
+        MySqlConnection Connection = new MySqlConnection(ConnectionString);
+        bd_gestionEntities bd = new bd_gestionEntities();
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult IndexCol()
+        {
+            return View(bd.collaborateur.Where(m=>m.FLAGEVAL==false).ToList());
+        }
         // GET: Evaluation/Details/5
         public ActionResult Details(int id)
         {
